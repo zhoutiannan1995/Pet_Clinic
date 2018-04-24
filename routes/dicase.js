c8e5f728af6be6f600c8ad54765898ff66ba7c45
Zhoutiannan <704785120@qq.com>
2018-04-25 03:06:24 +0800

    update code

--- routes/dicase.js
+++ routes/dicase.js
@@ -82,21 +82,68 @@ router.post('/addDicase', Auth, upload.fields([{name:'file'},{name:'video'}]), f
 });
 
 //修改病例
-router.post('/modifyDicase', Auth, function (req, res) {
-  let dicase = req.body.data,
-    sql = `UPDATE pcdb.pc_dicase SET diagnosis_des='${dicase.diagnosis_des}',diagnosis_pic='${dicase.diagnosis_pic}',diagnosis_video='${dicase.diagnosis_video}',treatment_des='${dicase.treatment_des}',diname_id=${dicase.diname_id} WHERE dicase_id='${dicase.dicase_id}'`;
-  connection.query(sql, function (err, result) {
-    if (err) res.send({ code: '999', msg: err });
-    else {
-      console.log("修改病例成功!dicase_id:", dicase.dicase_id);
-      res.send({ code: '000', data: result });
-    }
+// router.post('/modifyDicase', Auth, function (req, res) {
+//   let dicase = req.body.data,
+//     sql = `UPDATE pcdb.pc_dicase SET diagnosis_des='${dicase.diagnosis_des}',diagnosis_pic='${dicase.diagnosis_pic}',diagnosis_video='${dicase.diagnosis_video}',treatment_des='${dicase.treatment_des}',diname_id=${dicase.diname_id} WHERE dicase_id='${dicase.dicase_id}'`;
+//   connection.query(sql, function (err, result) {
+//     if (err) res.send({ code: '999', msg: err });
+//     else {
+//       console.log("修改病例成功!dicase_id:", dicase.dicase_id);
+//       res.send({ code: '000', data: result });
+//     }
+//   });
+// });
+//修改病例
+router.post('/modifyDicase', Auth, upload.fields([{name:'file'},{name:'video'}]), function (req, res) {
+  //upload.fields([{name:'file'},{name:'text'}]), function (req, res) {
+    console.log("filerewrwrewrewrewre",req.files.file[0]);
+    console.log("filerewrwrewrewrewre",req.files.video[0]);
+    console.log("body888888e4239493294929924",req.body);
+    let dicase = req.body;
+    //删除已有图像视频文件
+    del(dicase.dicase_name);
+    let arr = req.files.file[0].originalname.split('.');
+    let newPicName = dicase.dicase_name + '.' + arr[arr.length-1];
+        arr = req.files.video[0].originalname.split('.');
+    let newVidName = dicase.dicase_name + '.' + arr[arr.length-1];
+    console.log(newPicName);
+    let sql = `UPDATE pcdb.pc_dicase SET diagnosis_des='${dicase.diagnosis_des}',diagnosis_pic='${newPicName}',diagnosis_video='${newVidName}',treatment_des='${dicase.treatment_des}',diname_id=${dicase.diname_id} WHERE dicase_id='${dicase.dicase_id}'`;
+  
+    let tmp_path = req.files.file[0].path;
+    let tmp_path2 = req.files.video[0].path;
+    let target_path = 'public/images/' + newPicName;
+    let target_path2 = 'public/videos/' + newVidName;
+    connection.query(sql, function (err, result) {
+      if (err) res.send({ code: '999', msg: err });
+      else {
+        let src = fs.createReadStream(tmp_path);
+        let dest = fs.createWriteStream(target_path);
+        src.pipe(dest);
+        src.on('end', function() {
+          let src = fs.createReadStream(tmp_path2);
+          let dest = fs.createWriteStream(target_path2);
+          src.pipe(dest);
+          src.on('end', function() {
+            console.log("修改病例成功!dicase_name:", dicase.dicase_name);
+            res.send({ code: '000', data: result }); 
+          });
+          src.on('error', function(err) {
+            res.send({ code: '999', msg: '视频保存失败！'}); 
+          });
+        });
+        src.on('error', function(err) {
+          res.send({ code: '999', msg: '图像保存失败！'}); 
+        });
+      }
+    });
+    res.send({code:'000', msg:'成功'});
   });
-});
+
 //删除病例
 router.post('/delDicase', Auth, function (req, res) {
   let dicase = req.body.data,
     sql = `DELETE FROM pcdb.pc_dicase WHERE dicase_id='${dicase.dicase_id}'`;
+  del(dicase.dicase_name);
   connection.query(sql, function (err, result) {
     if (err) res.send({ code: '999', msg: err });
     else {
@@ -105,5 +152,25 @@ router.post('/delDicase', Auth, function (req, res) {
     }
   });
 });
+
+function del(name){
+  fs.readdir('../public/images', function (error, files){
+    if (error) throw error;
+    var cc = files.filter(function (fileName){
+      let reg = new RegExp(name+'.');
+      return reg.test(fileName);
+    })[0];
+    fs.unlinkSync('../public/images/'+cc);
+  });
+  fs.readdir('../public/videos', function (error, files){
+    if (error) throw error;
+    var cc = files.filter(function (fileName){
+      let reg = new RegExp(name+'.');
+      return reg.test(fileName);
+    })[0];
+    fs.unlinkSync('../public/videos/'+cc);
+  });
+}
+
 
 module.exports = router;
